6c9e403d33850f240c304b06c9eb3278d7245b13
TAISNAPOLEAO <taisnapoleao@hotmail.com>
2022-12-30 14:10:14 -0300

    TrilhaJavaModulo3
    TrilhaJavaModulo3

---
+++ aula07/Erro01.java
@@ -0,0 +1,11 @@
+class Erro01{
+
+	public static void main(String [] args){
+	
+		//Erro de sintaxe
+		System.out.println("Hello World!!)
+	
+	}
+	
+
+}
---
+++ aula07/Erro02.java
@@ -0,0 +1,12 @@
+class Erro02{
+
+	public static void main(String [] args){
+	
+		//Erro de semantica
+		var int cont;
+		System.out.println("Voce Digitou: !!" + cont);
+	
+	}
+	
+
+}
