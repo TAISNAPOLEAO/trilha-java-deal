bee3f6dead8baaef8e942f22d17d486ff917427d
TAISNAPOLEAO <taisnapoleao@hotmail.com>
2022-12-30 14:08:23 -0300

    TrilhaJavaModulo3
    TrilhaJavaModulo3

---
+++ trilha-java-deal/src/aula07/Erro03.java
@@ -0,0 +1,11 @@
+class Erro03{
+
+	public static void Main(String [] args){
+	
+		//Erro em tempo de Execução com o nome main com letra maiscula "Main"
+		System.out.println("Voce Digitou: !!" );
+	
+	}
+	
+
+}
---
+++ trilha-java-deal/src/com/deal/trilhajava/aula11/VariaveisChar.java
@@ -0,0 +1,20 @@
+package com.deal.trilhajava.aula11;
+
+public class VariaveisChar {
+
+	public static void main(String[] args) {
+		
+		//char o = 'o';
+		//char i = 'i';
+		
+		//Tabela ASC
+		char o = 111;
+		char i = 105;
+		char interrogacao = 0x00E1; //valor = '?' unicode
+		
+		
+		System.out.println(""+o+i+interrogacao);
+
+	}
+
+}
