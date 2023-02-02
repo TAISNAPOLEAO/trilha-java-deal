3d0b253069eaa2f6b716612b98ade45101ebf1bd
TAISNAPOLEAO <taisnapoleao@hotmail.com>
2023-02-02 09:17:22 -0300

    continuacaodaslistas_
    continuacaodaslistascontinuacaodaslistas_

---
+++ com/deal/trilhajava/aula20/labs/Exer01.java
@@ -0,0 +1,17 @@
+package com.deal.trilhajava.aula20.labs;
+
+import java.util.Scanner;
+
+public class Exer01 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int [][] MatrizM = new int [4][4];
+		
+		
+
+	}
+
+}
